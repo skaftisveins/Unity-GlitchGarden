ab3256ef29486af9bc5d07c27166584f4109c438
ss1.h18@nemandi.ntv.is <ss1.h18@nemandi.ntv.is>
2019-09-19 21:28:17 +0000

    Next up: Attackers Striking Target

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -6,13 +6,20 @@ public class Attacker : MonoBehaviour {
 
     [Range(0f, 5f)]
     float currentSpeed = 1f;
+    GameObject currentTarget;
 
-	void Update () {
+    void Update () {
         transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
 	}
 
     public void SetMovementSpeed(float speed)
     {
         currentSpeed = speed; 
+    }
+
+    public void Attack(GameObject target)
+    {
+        GetComponent<Animator>().SetBool("isAttacking", true);
+        currentTarget = target;
     }
 }
